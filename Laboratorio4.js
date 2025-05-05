0d22f0f017d09de2a7fd7875211be54d23941274
Estudiante de Desarrollo de Software <71363459+JoAlejo09@users.noreply.github.com>
2025-05-05 11:11:28 -0500

    Laboratorio update

--- Laboratorio4.js
+++ Laboratorio4.js
@@ -6,7 +6,7 @@
         id: "1234567890",
         nombre: "Juan Pérez",
         correo: "juan.perez@facebook.com",
-        fechaNacimiento: "1990-05-05",
+        fechaNacimiento: "2007-05-05",
         genero: "masculino",
         fotoPerfil: "https://facebook.com/foto_perfil_juan.jpg",
         amigos: [
@@ -17,7 +17,7 @@
           {
             id: "post001",
             contenido: "¡Hola a todos! Este es mi primer post.",
-            fecha: "2025-05-01",
+            fecha: "2025-04-01",
             reacciones: {
               meGusta: 20,
               meEncanta: 5,
@@ -30,7 +30,7 @@
           }
         ],
         estado: "Conectado",
-        ubicacion: "Ciudad de México",
+        ubicacion: "Ecuador",
         biografia: "Amante de la tecnología y el café.",
         intereses: ["Programación", "Viajes", "Fotografía"]
       };
@@ -45,7 +45,7 @@
           correo: "juan.perez@facebook.com",
           fotoPerfil: "https://facebook.com/foto_juan.jpg",
           amigos: ["002", "003"],
-          estado: "Conectado"
+          estado: true
         },
         {
           id: "002",
@@ -53,7 +53,7 @@
           correo: "ana.lopez@facebook.com",
           fotoPerfil: "https://facebook.com/foto_ana.jpg",
           amigos: ["001"],
-          estado: "Desconectado"
+          estado: false
         },
         {
           id: "003",
@@ -61,8 +61,20 @@
           correo: "carlos.diaz@facebook.com",
           fotoPerfil: "https://facebook.com/foto_carlos.jpg",
           amigos: ["001"],
-          estado: "Conectado"
+          estado: true
         }
       ];
       
-      console.log(usuariosFacebook);
+      console.log(usuariosFacebook); // Sin cambios
+
+      /*EJECUCION DE PROPIEDADES Y VALORES */
+      /*CON LOS OBJETOS */
+      /*Actualiza el valor del correo del usuario de Facebook */ usuarioFacebook.correo = "Juan.perez@gmail.com"
+      /*Eliminamos la biografia ingresada por el usuario */ delete usuarioFacebook.biografia
+      console.log(usuarioFacebook) //Impresion de los cambios realizados
+      /*Mostramos informacion determinada del usuario */ 
+      const{id,nombre:usuario, correo:email,fechaNacimiento} = usuarioFacebook      
+      console.log(`El usuario de Facebook \n Nombre: ${usuario}\nEdad:${email}\nFecha de Nacimiento:${fechaNacimiento}`)
+      /* CON LOS ARREGLOS */
+      const user = usuariosFacebook.filter(p=> p.nombre==="Carlos Diaz") //Devuelve el valor del arreglo con el usuario Carlos Diaz
+      console.log (usuariosFacebook.estado.includes(true)) //Devuelve los cuantos usuarios estan activos | true 
